e04490e3fee20c13079e4df415ee83f401b6112b
boobacar <boubsfal@gmail.com>
2025-08-27 20:28:50 +0000

    changing footer copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className=" w-full py-4 bg-amber-900 from-blue-100 via-white to-pink-100 text-center text-white shadow-inner">
-      © <b>Teliman Share</b>
+      © <b>Teliman Logistique</b>
     </footer>
   );
 }
